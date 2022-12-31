a108deadfdf74d080d951f3b60e0f5c832b554c0
unknown <zcfshr@gmail.com>
2022-12-31 14:25:31 -0700

    clean up

--- server/models/Music.js
+++ server/models/Music.js
@@ -1,8 +1,8 @@
 const { Schema } = require('mongoose');
 
-// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedArtists` array in User.js
 const musicSchema = new Schema({
-  authors: [
+  artists: [
     {
       type: String,
     },
@@ -12,7 +12,7 @@ const musicSchema = new Schema({
     required: true,
   },
   // saved book id from GoogleBooks
-  bookId: {
+  artistId: {
     type: String,
     required: true,
   },
--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 // import schema from Book.js
-const bookSchema = require('./Artist');
+const bookSchema = require('./Music');
 
 const userSchema = new Schema(
   {
